16f175be1e7de12125a4bca2e694335f7f24d2c4
Aprendiz de Sysadmin <33311812+aprendizdesys@users.noreply.github.com>
2017-11-15 09:54:35 +0100

    Add files via upload

---
+++ Test-Conection/Ping-Servidores.ps1
@@ -0,0 +1,14 @@
+﻿# Leemos el archivo 
+$listado = get-content .\list_servidores.txt
+
+#Para cada ip del archivo 
+ForEach ($ip in $listado) {
+    if (test-connection -ComputerName $ip -Count 1 -Quiet) { 
+        # Si el test-connection es correcto
+        write-host -BackgroundColor DarkGreen -ForegroundColor Black ('{0} - RESPONDE' -f $ip)
+        } else {   
+          # Si el test-connection no es correcto
+          Write-Host -BackgroundColor DarkRed -ForegroundColor White ('{0} - NO RESPONDE' -f $ip)
+        }
+}
+
