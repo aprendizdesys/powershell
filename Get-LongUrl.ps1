bbd2867ba0654c7c8cefb616008bf72f6f9089c8
Aprendiz de Sysadmin <33311812+aprendizdesys@users.noreply.github.com>
2018-01-02 13:43:00 +0100

    Nuevo Get-LongUrl

---
+++ Get-LongUrl.ps1
@@ -0,0 +1,26 @@
+﻿# Autor: Francisco Palenzuela Luque @fpalenzuela
+# Fecha: 02/01/2017
+
+
+<#
+.SYNOPSIS
+  Convierte una url acortada en su url real.
+
+.EXAMPLE 
+\Get-LongUrl.ps1  http://alturl.com/3gpn9
+
+.LINK    
+ Post del script: https://aprendizdesysadmin.com/powershell_-mini-tip-obteniendo-la-url-completa-de-un-enlace-corto/
+#>
+
+[CmdletBinding()]
+     param ( 
+       # El parametro es obligatorio y el pattern es para direcciones Ipv4.
+       [Parameter(Mandatory=$true)] 
+       [string]$ShortUrl
+
+     )
+
+
+$LongUrl = (Invoke-WebRequest -Uri $ShortUrl).BaseResponse.ResponseUri.OriginalString
+Write-host ('La url larga es: {0}' -f $LongUrl)
